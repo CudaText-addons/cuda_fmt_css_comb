c9c13bb996db6c4d77bf96e330a708a9e91a43a8
Alexey-t <support@uvviewsoft.com>
2019-07-13 17:56:19 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,30 @@
+import os
+import json
+from cudax_nodejs import run_node
+from cuda_fmt import get_config_filename
+import cudatext as app
+
+tool_js = os.path.join(os.path.dirname(__file__), 'csscomb.js')
+
+def get_config():
+    fn = get_config_filename('CSS Comb')
+    text = open(fn).read()
+    d = json.loads(text)
+    return json.dumps(d)
+
+def do_format_ex(text, syntax):
+    return run_node(text, [tool_js, syntax, get_config()])
+
+    #try:
+    #    return run_node(text, [tool_js, get_syntax(), get_config()])
+    #except Exception as e:
+    #    app.msg_box('Error while running Node.js \n'+str(e), app.MB_OK+app.MB_ICONERROR)
+
+def do_format_css(text):
+    return do_format_ex(text, 'css')
+
+def do_format_scss(text):
+    return do_format_ex(text, 'scss')
+
+def do_format_less(text):
+    return do_format_ex(text, 'less')
---
+++ readme/readme.txt
@@ -0,0 +1,8 @@
+Formatter "CSS Comb".
+Requires Node.js, uses CSScomb engine to sort CSS, SCSS, LESS properties in specific order. 
+
+You can configure it in json file.
+Doc about config https://github.com/csscomb/csscomb.js/blob/master/doc/options.md
+
+Author: Alexey Torgashin (CudaText)
+License: MIT
