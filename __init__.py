08cccbcd551ede5ee1a309d8924c53886230a2b6
Alexey-t <support@uvviewsoft.com>
2019-07-13 18:19:45 +0300

    upd lib

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 from cudax_nodejs import run_node
 from cuda_fmt import get_config_filename
@@ -8,23 +9,27 @@ tool_js = os.path.join(os.path.dirname(__file__), 'csscomb.js')
 
 def get_config():
     fn = get_config_filename('CSS Comb')
-    text = open(fn).read()
-    d = json.loads(text)
+    s = open(fn, 'r').read()
+    #del // comments
+    s = re.sub(r'(^|[^:])//.*', r'\1', s)   
+    d = json.loads(s)
     return json.dumps(d)
 
 def do_format_ex(text, syntax):
-    return run_node(text, [tool_js, syntax, get_config()])
-
-    #try:
-    #    return run_node(text, [tool_js, get_syntax(), get_config()])
-    #except Exception as e:
-    #    app.msg_box('Error while running Node.js \n'+str(e), app.MB_OK+app.MB_ICONERROR)
+    config = get_config()
+    try:
+        return run_node(text, [tool_js, syntax, config, '.'])
+    except Exception as e:
+        app.msg_box('Error while running Node.js \n'+str(e), app.MB_OK+app.MB_ICONERROR)
 
 def do_format_css(text):
     return do_format_ex(text, 'css')
 
 def do_format_scss(text):
     return do_format_ex(text, 'scss')
+
+def do_format_sass(text):
+    return do_format_ex(text, 'sass')
 
 def do_format_less(text):
     return do_format_ex(text, 'less')
--- readme/readme.txt
+++ readme/readme.txt
@@ -1,8 +1,11 @@
 Formatter "CSS Comb".
-Requires Node.js, uses CSScomb engine to sort CSS, SCSS, LESS properties in specific order. 
+Requires Node.js, uses CSScomb engine to sort properties in lexers CSS, SCSS, Sass, LESS. 
 
-You can configure it in json file.
-Doc about config https://github.com/csscomb/csscomb.js/blob/master/doc/options.md
+You can configure it in json file. Doc about config:
+https://github.com/csscomb/csscomb.js/blob/master/doc/options.md
+
+Uses portions from Sublime Text plugin:
+https://github.com/csscomb/sublime-csscomb/
 
 Author: Alexey Torgashin (CudaText)
 License: MIT
